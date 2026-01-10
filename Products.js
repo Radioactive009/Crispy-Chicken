b030138c12a149cdf61bba53d4b0c28f4469c320
Kislay Kumar <kislayreigns20092002@gmail.com>
2026-01-10 14:01:14 +0530

    Add files via upload

---
+++ Products.js
@@ -0,0 +1,10 @@
+// Product data from JSON with images
+// Hamburger menu toggle
+ const hamburger = document.getElementById("hamburger");
+  const navMenu = document.getElementById("primary-navigation");
+
+  hamburger.addEventListener("click", () => {
+    const expanded = hamburger.getAttribute("aria-expanded") === "true";
+    hamburger.setAttribute("aria-expanded", String(!expanded));
+    navMenu.classList.toggle("active");
+  });
